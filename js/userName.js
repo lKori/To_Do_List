972acb8fc847709a6d929198d53c77889982f27f
lKori <yuy0841@gmail.com>
2022-04-20 21:27:23 +0900

    20220420_login
    사용자 이름을 입력받아 localstorage에 저장한 뒤 to do list 페이지로 이동

---
+++ js/userName.js
@@ -0,0 +1,40 @@
+const login = document.querySelector("#login-form");
+const userName = document.querySelector("#userName");
+const submitName = document.querySelector("#submitName");
+const h1 = document.querySelector("h1");
+
+const USER_KEY = "user";
+
+function movePage() {
+    location.href = "./html/toDoList.html";
+}
+
+function inputUserName(event) {
+    event.preventDefault();
+
+    if (event.target[0].value) {
+        localStorage.setItem("user", event.target[0].value);
+        helloUser();
+    } else {
+        alert("Please write your name!");
+    }
+}
+
+function helloUser() {
+    const name = localStorage.getItem(USER_KEY);
+
+    userName.classList.add("hidden");
+    submitName.classList.add("hidden");
+    h1.innerText = `Welcome ${name}!`;
+    setTimeout(movePage, 2000);
+}
+
+function setUser() {
+    if (localStorage.getItem(USER_KEY)) {
+        helloUser();
+    } else {
+        login.addEventListener("submit", inputUserName);
+    }
+}
+
+setUser();
