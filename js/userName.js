3378bfb06247268761db9dea5f650a96666f0046
lKori <yuy0841@gmail.com>
2022-04-24 02:17:49 +0900

    20220424

--- js/userName.js
+++ js/userName.js
@@ -6,35 +6,35 @@ const h1 = document.querySelector("h1");
 const USER_KEY = "user";
 
 function movePage() {
-    location.href = "./html/toDoList.html";
+  location.href = "./html/toDoList.html";
 }
 
 function inputUserName(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (event.target[0].value) {
-        localStorage.setItem("user", event.target[0].value);
-        helloUser();
-    } else {
-        alert("Please write your name!");
-    }
+  if (event.target[0].value) {
+    localStorage.setItem("user", event.target[0].value);
+    helloUser();
+  } else {
+    alert("Please write your name!");
+  }
 }
 
 function helloUser() {
-    const name = localStorage.getItem(USER_KEY);
+  const name = localStorage.getItem(USER_KEY);
 
-    userName.classList.add("hidden");
-    submitName.classList.add("hidden");
-    h1.innerText = `Welcome ${name}!`;
-    setTimeout(movePage, 2000);
+  userName.classList.add("hidden");
+  submitName.classList.add("hidden");
+  h1.innerText = `Welcome ${name}!`;
+  setTimeout(movePage, 2000);
 }
 
 function setUser() {
-    if (localStorage.getItem(USER_KEY)) {
-        helloUser();
-    } else {
-        login.addEventListener("submit", inputUserName);
-    }
+  if (localStorage.getItem(USER_KEY)) {
+    helloUser();
+  } else {
+    login.addEventListener("submit", inputUserName);
+  }
 }
 
 setUser();
